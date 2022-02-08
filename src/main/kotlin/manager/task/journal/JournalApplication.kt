ecf640253b3021dacc438c26f4cd8c8c9b679363
TIUNCHIK Maksim N <maksim.tiunchik@raiffeisen.ru>
2022-02-09 00:29:36 +0300

    init commit

---
+++ src/main/kotlin/manager/task/journal/JournalApplication.kt
@@ -0,0 +1,11 @@
+package manager.task.journal
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class MainTaskJournalApplication
+
+fun main(args: Array<String>) {
+	runApplication<MainTaskJournalApplication>(*args)
+}
---
+++ src/main/kotlin/manager/task/journal/JournalStarterConfiguration.kt
@@ -0,0 +1,9 @@
+package manager.task.journal
+
+import org.springframework.context.annotation.ComponentScan
+import org.springframework.context.annotation.Configuration
+
+@Configuration
+@ComponentScan(basePackages = ["manager.task.journal"])
+class JournalStarterConfiguration {
+}
